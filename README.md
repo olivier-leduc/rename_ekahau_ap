3b46a65bd9b9fef00c10093f19629d675e3f08d4
oleduc <oleduc@devvm583.vll0.facebook.com>
2020-08-27 19:57:33 -0700

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+# rename_ekahau_ap
---
+++ experimental/oleduc/utilities/rename_ekahau_apnames.py
@@ -0,0 +1,67 @@
+#!/usr/local/bin/python3
+# rename_ekahau_apnames.py
+#
+__author__  = "Olivier Le Duc"
+__version__ = "0.1"
+
+""" This script renames default Ekahau AP names into your own AP names, given a textfile containing a list of AP names to BSSIDs.
+    The *esx file is essentially a zip file that contains all the survey data (measurements, images, metadata).
+    This script will update entries in the accessPoints.json file and renames generically named APs ("Measured AP-XX:XX" into
+    AP names of your liking. The esx file needs to be unzipped prior to executing this script.
+
+    After running the script, you have zip all the files using your os zipping software (resulting zip file must not include
+    any other files than those originally included in the esx file). Change the extention of the newly zipped file to .esx
+    so that Ekahau pro can open the file. Once the project is opened in Ekahau, your AP names should show up.
+"""
+
+import json, os, sys
+import argparse
+
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return arg
+
+def initialize():
+    # Change to a generally reliable working directory.
+    os.chdir(os.environ['HOME'])
+
+    parser = argparse.ArgumentParser(description='some information',
+        usage='use "python %(prog)s --help" for more information',
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("-b", dest="bssidmapping", required=True,
+                        help="Input the full path of the file containing AP<->BSSID mapping.\nex: /Users/johnsmith/bssidmap.csv\nThe file should be using this format:\nap1,f0:72:ea:18:37:cd\nap2,f0:9f:c2:a4:67:e8", metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("-f", dest="esxfolder", required=True,
+                        help="Input the full path of the UNZIPPED esx file.\nex: /Users/johnsmith/warehousesurvey/", metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+    return parser
+
+
+def main():
+    with open(args.bssidmapping) as f:
+      bssidmapping_list = f.readlines()
+      bssidmapping = {}
+      for line in bssidmapping_list:
+        row = line.strip().split(',')
+        bssidmapping[row[0]] = row[1]
+
+    with open(os.path.join(args.esxfolder, 'accessPoints.json')) as f:
+      esxdata = json.load(f)
+
+    for apname, bssid in bssidmapping.items():
+      for ap in esxdata['accessPoints']:
+        if bssid[-5:] in ap['name'] :
+          ap['name'] = ap['name'].replace(ap['name'], apname)
+
+
+    with open(os.path.join(args.esxfolder, 'accessPoints.json'), 'w') as f:
+      json.dump(esxdata, f, indent=4)
+
+if __name__ == '__main__':
+    parser = initialize()
+    args = parser.parse_args()
+    main()
+
